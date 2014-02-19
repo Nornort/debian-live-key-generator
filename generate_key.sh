a293ec9dc13edde788c79255e378f7e6d5af4e46
ksamak <ksamak@riseup.net>
2014-02-19 10:44:56 +0100

    source files

---
+++ generate_key.sh
@@ -0,0 +1,270 @@
+#!/bin/bash
+
+##############################################
+##title           : generateLiveImage.sh
+##description     : this script builds a debian live image, then writes it on a given media
+##author          : ksamak
+##Date            : 2014/01/27
+##Version         : 1.0.0
+##usage           : generateLiveImage.sh
+##notes           : Install live-build cdebootstrap parted to use this script
+##############################################
+
+
+
+usage() {
+    echo "should display usage"
+    exit 0;
+}
+
+version() {
+    echo "0.2"
+    exit 0
+}
+
+##################
+## ARG PARSING
+while [[ $# > 0 ]]; do
+    key="$1"
+    shift;
+    case $key in
+        -v|--verbose) DEBUG=YES;;
+        --version) vers=YES;;
+        -i|--input) file="$1"; shift;;
+        -s|--splash) splash_image="$1"; shift;;
+        --no-build) no_build=YES;;
+        --no-write) no_write=YES;;
+        -q|--quiet) quiet=YES;;
+        *) if [ -n "$media" ]; then
+           usage;
+           else
+           media="$key"; 
+           fi;; # positionnal arg
+    esac
+done
+##################
+
+## support functions ##
+NORMAL=$(tput -T xterm sgr0)
+GREEN=$(tput -T xterm setaf 2; tput -T xterm bold)
+YELLOW=$(tput -T xterm setaf 3)
+RED=$(tput -T xterm setaf 1)
+
+red() {
+    echo "$RED$*$NORMAL"
+}
+green() {
+    echo "$GREEN[INFO]  $*$NORMAL"
+}
+yellow() {
+    echo "$YELLOW[INFO]  $*$NORMAL"
+}
+debug() {
+    [ "$DEBUG" ] && echo "$YELLOW[DEBUG] $NORMAL$*";
+}
+debug "debug on"
+
+check() {
+    if [ ${?} -ne 0 ]; then red "[ERROR] $*"; exit 1; fi
+}
+
+# you should exec "apt-get install live-build cdebootstrap parted" if not done TODO
+
+watch_splash() {
+    while true; do
+        stat "binary/isolinux/splash.png" 2> /dev/null
+        if [ ${?} = 0 ]; then
+            red "copying $1 to binary/isolinux/splash.png NOW!";
+            cp $1 binary/isolinux/splash.png;
+            break
+        fi
+        sleep 0.2
+    done
+}
+
+killAndQuit() {
+    kill -9 $!
+    red "killing loop and existing"
+    exit 0
+}
+
+build_image() {
+    yellow "cleaning potential previous builds"
+    ## cleaning in case of previous build ##
+    debug "rm -rf auto .build chroot* config local *.iso* binary.* binary*"
+    rm -rf auto .build chroot* local *.iso* binary.* config binary*
+    if [ ${?} -ne 0 ]; then debug "removal of previous build went wrong."; fi
+    debug "removal ok."
+
+    ############################################################
+    ######### Image configuration. choose your options #########
+    yellow "configuring build"  # for more info on building options, run man lb_config
+    lb config \
+        --binary-images iso-hybrid \
+        --apt aptitude \
+        --architecture i386 \
+        --bootappend-live 'boot=live config quiet splash persistence' \
+        --firmware-chroot true \
+        --archive-area "main contrib non-free" \
+        --firmware-binary true \
+        --backports true \
+        --updates true \
+        --distribution wheezy \
+        --apt-recommends true \
+
+    check "Config of image failed."
+    green "build configured"
+
+##################################################
+############## CONFIG THAT WORK!! ################
+#        --binary-images iso-hybrid \
+#        --apt aptitude \
+#        --architecture i386 \
+#        --bootappend-live 'boot=live config quiet splash persistence' \
+##################################################
+#        --bootappend-live 'boot=live config quiet splash persistence' \
+#        --binary-images iso-hybrid \
+#        --apt aptitude \
+#        --architecture i386 \
+#        --firmware-chroot true \
+#        --archive-area "main contrib non-free" \
+#        --firmware-binary true \
+#        --backports true \
+#        --updates true \
+#        --apt aptitude \
+#        --grub-splash "splash.png" \
+#        --distribution wheezy \
+#        --apt-recommends true \
+##################################################
+##################################################
+
+
+    ####################################################################
+    ########## Packages section. choose wich packages you like #########
+    ## Debian recommends the maintained metapackages beginning by "task-"
+    yellow "setting installation packages"
+    # vital section
+    echo "vim most tmux openssh-server"     >> config/package-lists/system.list.chroot
+    check "failed to set install packages."
+    echo "htop"     >> config/package-lists/system.list.chroot
+    echo "cryptsetup ecryptfs-utils"                   >> config/package-lists/system.list.chroot
+    # desktop section
+    #echo "task-french-desktop task-french"  >> config/package-lists/desktop.french.list.chroot # for the french
+    echo "task-xfce-desktop"                >> config/package-lists/desktop.xfce.list.chroot
+    echo "vlc"                              >> config/package-lists/desktop.multimedia.list.chroot
+    echo "gparted"                          >> config/package-lists/desktop.tools.list.chroot
+    # non-free section
+    echo "flashplugin-nonfree"              >> config/package-lists/desktop.non-free.list.chroot
+    green "packaging configured"
+
+
+    #########################################
+    ###### set the optional watch loop ######
+
+    if [ -n "$splash_image" ]; then
+        stat $splash_image > /dev/null
+        check "ooops, can't reach selected splash image?"
+        debug "mkdir binary/isolinux"
+        watch_splash $splash_image &
+        trap "killAndQuit" SIGINT
+    fi
+
+    ##############################
+    ######### then build #########
+    yellow "building"
+    if [ -n "$DEBUG" ]; then
+        lb build;
+    else
+        lb build --quiet > /dev/null;
+    fi
+    # abort and kill optional loop subshell.
+    if [ ${?} -ne 0 ]; then
+        red "[ERROR] Build of image failed";
+        if [ -n "$splash_image" ]; then
+            kill -9 $!; # kill the watch loop.
+        fi
+        exit 1;
+    fi
+
+    if [ -n "$splash_image" ]; then
+        kill -9 $!; # kill the watch loop.
+        trap - SIGINT;
+    fi
+
+    green "build complete"
+}
+
+write_image() {
+    debug "writing image"
+    yellow "checking parameters for image write"
+    stat $media > /dev/null
+    check "selected device does not exist."
+    stat binary.hybrid.iso  > /dev/null
+    check "couldn't find hybrid iso to write."
+    green "ready to write media"
+
+    if [ -z "$quiet" ]; then 
+        yellow "about to override partitions on $media, do you wish to continue?"
+        read -p "type \"yes\" to continue: " CONFIRM
+        if [ "$CONFIRM" != "yes" ]; then exit 0; fi
+    else
+        debug "not asking for permission to write on media"
+    fi
+    yellow "overwriting partitions on $media"
+    debug "dd if=binary.hybrid.iso of=$media"
+    dd if=binary.hybrid.iso of=$media
+    check "unable to write image on media, you may need root priviledges. (note: use --no-build to bypass build and write directly)"
+    green "base image written to media"
+
+    PART1_SIZE=$(parted -m $media unit s print | grep "boot\, hidden" | cut -d ":" -f 3 | cut -d "s" -f 1)
+    check "couldn't read media partition size"
+    PART2_BEGINNING=$(($PART1_SIZE+1))
+    PART2_END=$(parted -m $media unit s print | grep "$media" | cut -d ":" -f 2 | cut -d "s" -f 1)
+    check "couldn't read media key size"
+    PART2_END=$(($PART2_END-1))
+
+    debug "parted -a optimal -s $media unit s mkpart primary $PART2_BEGINNING $PART2_END"
+    yellow "overrideing partitions on $media"
+    parted -a optimal -s $media unit s mkpart primary $PART2_BEGINNING $PART2_END
+    debug "mkfs.ext4 -L persistence $media$((2))"
+    mkfs.ext4 -L persistence $media$((2))
+    check "unable to create ext4 filesystem on media"
+    green "persistence filesystem created"
+
+    mkdir mounted
+    check "unable to create folder to mount media"
+    yellow "mounting partition to insert persistence.conf"
+    debug "mount -t ext4 $media$((2)) mounted"
+    mount -t ext4 $media$((2)) mounted
+    echo "/ union" > mounted/persistence.conf
+    #echo "/ union" >> mounted/persistence.conf ## feel free to customize the folders.
+    yellow "mounted persistence partition and set persistence folders"
+    umount mounted
+    rmdir mounted
+    green "persistence file set"
+    green "image sucessfully written."
+
+}
+
+if [ -n "$file" ]; then
+    file=binary.hybrid.iso;
+fi
+
+if [ -n "$vers" ]; then
+    version;
+fi
+
+if [ -z "$no_build" ]; then
+    build_image;
+else
+    yellow "not building image";
+fi
+
+if [ -z "$no_write" ]; then
+    write_image;
+else
+    yellow "not writing image";
+fi
+
+exit 0
+
